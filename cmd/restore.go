c48ba384ce76f689506d9d36d3432ab6a4547035
Justin Waite <justindwaite@gmail.com>
2024-12-28 09:57:08 -0700

    fix: use a temp dir and rsync to deal with existing dir issues

--- cmd/restore.go
+++ cmd/restore.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"os"
 	"os/exec"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -18,11 +19,30 @@ var restoreCommand = &cobra.Command{
 	Short: "Restores a dotfiles repository onto the current machine",
 	Long:  `Initalizes dotbulter by creating a detached git repository in the user's home directory.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		tempDirectory := filepath.Join(os.TempDir(), "dotfiles")
 		dotfilesDir := viper.GetString("directory")
 		home, err := os.UserHomeDir()
 		cobra.CheckErr(err)
 
-		command := exec.Command("git", "clone", "--separate-git-dir", dotfilesDir, args[0], home)
+		command := exec.Command("git", "clone", "--separate-git-dir", dotfilesDir, args[0], tempDirectory)
+		command.Stdout = os.Stdout
+		command.Stderr = os.Stderr
+		command.Stdin = os.Stdin
+		command.Env = os.Environ()
+
+		err = command.Run()
+		cobra.CheckErr(err)
+
+		command = exec.Command("rsync", "--recursive", "--verbose", "--exclude", ".git", filepath.Join(tempDirectory+"/"), filepath.Join(home+"/"))
+		command.Stdout = os.Stdout
+		command.Stderr = os.Stderr
+		command.Stdin = os.Stdin
+		command.Env = os.Environ()
+
+		err = command.Run()
+		cobra.CheckErr(err)
+
+		command = exec.Command("rm", "-rf", tempDirectory)
 		command.Stdout = os.Stdout
 		command.Stderr = os.Stderr
 		command.Stdin = os.Stdin
